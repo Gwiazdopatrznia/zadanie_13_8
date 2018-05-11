c8b28bc5a3a9fb4766d09ef054967df5866ac43b
Anna Gajewska <gajewska.anna@o2.pl>
2018-05-11 17:35:20 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('index.html', 'utf-8', function(err, data){
+        	response.write(data);
+        	response.end();
+        });
+
+    } else {
+        response.statusCode = 404;
+        fs.readFile("404.jpg", "binary", function (err, data) {
+        	if (err) throw err;
+        	response.writeHead(200, {"Content-Type": "jpg"});
+        	response.write(data, "binary");
+            response.end();
+        });  
+    }
+});
+
+server.listen(8080);
